0fde26e00fa55420df0410fe3edee8ed3c56e794
Diego Moraes <diegobmcenter@gmail.com>
2026-08-14 13:02:31 -0300

    BMCenter v10.4.55 - pecas inline layout aprovado

--- PUBLICAR-ATUALIZACAO.bat
+++ PUBLICAR-ATUALIZACAO.bat
@@ -1,12 +1,12 @@
 @echo off
 setlocal EnableExtensions
 chcp 65001 >nul
-title BMCenter Smartphones v10.4.54 - Publicar Atualizacao
+title BMCenter Smartphones v10.4.55 - Publicar Atualizacao
 cd /d "%~dp0"
 
 set "REPO=https://github.com/diegobmcenter/BMCenter-Smartphones.git"
 set "BRANCH=main"
-set "VERSION=10.4.54"
+set "VERSION=10.4.55"
 
 echo.
 echo ============================================================
@@ -67,7 +67,7 @@ echo [7/8] Criando a atualizacao...
 git add -A
 git diff --cached --quiet
 if not errorlevel 1 goto :no_changes
-git commit -m "BMCenter v10.4.54 - pecas inline layout aprovado"
+git commit -m "BMCenter v10.4.55 - pecas inline layout aprovado"
 if errorlevel 1 goto :git_operation_error
 
 echo [8/8] Enviando ao GitHub...
