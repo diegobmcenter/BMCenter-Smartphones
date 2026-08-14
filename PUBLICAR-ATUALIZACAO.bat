f16380254255e8b88bc3b530ea3bee489fbac8d8
Diego Moraes <diegobmcenter@gmail.com>
2026-08-14 17:18:34 -0300

    BMCenter v10.4.63 - mobile historico pecas e multi-pedidos

--- PUBLICAR-ATUALIZACAO.bat
+++ PUBLICAR-ATUALIZACAO.bat
@@ -2,11 +2,11 @@
 setlocal EnableExtensions
 cd /d "%~dp0"
 
-title BMCenter Smartphones v10.4.62 - Publicar Atualizacao
+title BMCenter Smartphones v10.4.63 - Publicar Atualizacao
 
 set "REPO=https://github.com/diegobmcenter/BMCenter-Smartphones.git"
 set "BRANCH=main"
-set "VERSION=10.4.62"
+set "VERSION=10.4.63"
 
 echo.
 echo ============================================================
@@ -73,7 +73,7 @@ git add -A
 if errorlevel 1 goto :git_operation_error
 git diff --cached --quiet
 if not errorlevel 1 goto :no_changes
-git commit -m "BMCenter v10.4.62 - mobile historico pecas e multi-pedidos"
+git commit -m "BMCenter v10.4.63 - mobile historico pecas e multi-pedidos"
 if errorlevel 1 goto :git_operation_error
 
 echo [8/8] Enviando ao GitHub...
