5eadc5ceec0fde5c70564b581b58472703b14126
Diego Moraes <diegobmcenter@gmail.com>
2026-08-14 15:09:31 -0300

    BMCenter v10.4.58 - modal aparelho opcao 2 aprovada

--- PUBLICAR-ATUALIZACAO.bat
+++ PUBLICAR-ATUALIZACAO.bat
@@ -1,12 +1,12 @@
 @echo off
 setlocal EnableExtensions
 chcp 65001 >nul
-title BMCenter Smartphones v10.4.57 - Publicar Atualizacao
+title BMCenter Smartphones v10.4.58 - Publicar Atualizacao
 cd /d "%~dp0"
 
 set "REPO=https://github.com/diegobmcenter/BMCenter-Smartphones.git"
 set "BRANCH=main"
-set "VERSION=10.4.57"
+set "VERSION=10.4.58"
 
 echo.
 echo ============================================================
@@ -67,7 +67,7 @@ echo [7/8] Criando a atualizacao...
 git add -A
 git diff --cached --quiet
 if not errorlevel 1 goto :no_changes
-git commit -m "BMCenter v10.4.57 - modal aparelho opcao 2 aprovada"
+git commit -m "BMCenter v10.4.58 - modal aparelho opcao 2 aprovada"
 if errorlevel 1 goto :git_operation_error
 
 echo [8/8] Enviando ao GitHub...
