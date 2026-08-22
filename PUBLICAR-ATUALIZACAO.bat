3c6e9f7ed79010301801b7d1bc36f51c7cbba71e
Diego Moraes <diegobmcenter@gmail.com>
2026-08-22 03:45:19 -0300

    BMCenter v10.5.33 - corrige contrato dos filtros e mantem passada fina

--- PUBLICAR-ATUALIZACAO.bat
+++ PUBLICAR-ATUALIZACAO.bat
@@ -2,11 +2,11 @@
 setlocal EnableExtensions EnableDelayedExpansion
 cd /d "%~dp0"
 
-title BMCenter Smartphones v10.5.43 - Publicar Atualizacao
+title BMCenter Smartphones v10.5.44 - Publicar Atualizacao
 
 set "REPO=https://github.com/diegobmcenter/BMCenter-Smartphones.git"
 set "BRANCH=main"
-set "VERSION=10.5.43"
+set "VERSION=10.5.44"
 
 echo.
 echo ============================================================
